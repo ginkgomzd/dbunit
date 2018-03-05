6756e3a3ea9e6c10058831e7f83c92a0926cb3c4
ginkgoMZD <mzd@ginkgostreet.com>
2018-03-05 10:30:23 -0500

    define getConnection on our own TestCase base class (per the manual)

---
+++ phpunit/tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DbUnitTests;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase {
+    // only instantiate pdo once for test clean-up/fixture load
+    private static $pdo = null;
+
+    // only instantiate PHPUnit_Extensions_Database_DB_IDatabaseConnection once per test
+    private $conn = null;
+
+    final public function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new \PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
+        }
+
+        return $this->conn;
+    }
+}
